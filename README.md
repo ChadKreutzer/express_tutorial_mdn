b51f9dc72c601f40ee90cf94af9a0432986e236c
Chad Kreutzer <kanthalion@gmail.com>
2017-08-09 12:37:59 +0000

    On part 5 book_detail.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MDN Express tutorial
 
 The Express tutorial from Mozilla Developers Network
+
+[Local Library Website](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Tutorial_local_library_website)
--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -16,14 +16,14 @@ var mongoose = require('mongoose');
 var mongoDB = 'mongodb://chad_kreutzer:password@ds055626.mlab.com:55626/local_library';
 mongoose.connect(mongoDB);
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:');
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog); //Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
